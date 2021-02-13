cf75982d57accf4c0ea6bd0bada25da5a1cae43a
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-02-13 15:12:47 +0530

    Changed how a new Core is created.

--- src/main/java/com/jacob/mips/models/Core.java
+++ src/main/java/com/jacob/mips/models/Core.java
@@ -5,25 +5,19 @@ import com.jacob.mips.models.instructions.InstructionBuilder;
 import java.util.function.Function;
 
 public class Core {
-    private final ProgramCounter programCounter;
-    private final MemoryArray memoryArray;
-    private final RegisterFile registerFile;
+	private final ProgramCounter programCounter;
+	private final MemoryArray    memoryArray;
+	private final RegisterFile   registerFile;
 
-    private final Function<ProgramInstructions, Core> createCoreWithInstructions = Core::new;
+	private static final Function<ProgramInstructions, Core> createCoreWithInstructions = Core::new;
 
-    public Core() {
-        this.programCounter = new ProgramCounter();
-        this.memoryArray = new MemoryArray();
-        this.registerFile = new RegisterFile();
-    }
+	private Core(ProgramInstructions instructions) {
+		this.programCounter = new ProgramCounter(instructions);
+		this.memoryArray = new MemoryArray();
+		this.registerFile = new RegisterFile();
+	}
 
-    private Core(ProgramInstructions instructions) {
-        this.programCounter = new ProgramCounter(instructions);
-        this.memoryArray = new MemoryArray();
-        this.registerFile = new RegisterFile();
-    }
-
-    public InstructionBuilder addInstructions() {
-        return new InstructionBuilder(createCoreWithInstructions);
-   }
+	public static InstructionBuilder newCoreWithInstruction() {
+		return new InstructionBuilder(createCoreWithInstructions);
+	}
 }
