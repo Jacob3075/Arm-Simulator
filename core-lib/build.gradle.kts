e2f0cabb011c77d96b2bc5908a487d637349041f
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-04-24 22:32:02 +0530

    Added kluent library and changed project name.

--- core-lib/build.gradle.kts
+++ core-lib/build.gradle.kts
@@ -10,6 +10,9 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    testImplementation ("org.amshove.kluent:kluent:1.65")
+    testImplementation ("org.jetbrains.kotlin:kotlin-test-junit:1.4.32")
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 
-rootProject.name = "ArmSim"
+rootProject.name = "ArmSimulator"
 
 include("core-lib")
