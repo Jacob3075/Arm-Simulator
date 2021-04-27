92748feac367488d0a6bf54e42b01f0118091796
Jacob Bosco <jacobs117a@gmail.com>
2021-04-27 10:14:09 +0000

    Added JetBrains Space build automation script

---
+++ .space.kts
@@ -0,0 +1,24 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Alternate day buildsat 10:00") {
+    startOn {
+        repository = "Arm-Sim"
+        gitPush { enabled = false }
+        schedule { cron("0 10 * * */2") }
+    }
+
+    container(displayName = "Arm Sim Container", image = "gradle:jdk11") {
+        resources {
+            cpu = 1.cpu
+            memory = 2500.mb
+        }
+        
+        kotlinScript { api ->
+            api.gradlew("build")
+        }
+    }
+}
