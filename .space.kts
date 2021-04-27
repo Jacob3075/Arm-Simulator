5b9ff324b6a707fa55e7a83d633d72f9df7978f9
Jacob Bosco <jacobs117a@gmail.com>
2021-04-27 15:38:52 +0000

    Simplified build script

--- .space.kts
+++ .space.kts
@@ -9,15 +9,5 @@ job("Alternate day buildsat 10:00") {
         gitPush { enabled = false }
         schedule { cron("0 10 * * */2") }
     }
-
-    container(displayName = "Arm Sim Container", image = "gradle:jdk11") {
-        resources {
-            cpu = 1.cpu
-            memory = 2500.mb
-        }
-        
-        kotlinScript { api ->
-            api.gradlew("build")
-        }
-    }
+    gradlew("openjdk:11", "build")
 }
