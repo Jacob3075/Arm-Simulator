1f39b03d1f3c58008166e67a01dd35483a8c3440
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-04-29 12:23:57 +0530

    Added equals and hashcode method for Word.

--- core-lib/src/main/kotlin/com/jacob/core_lib/word/Word.kt
+++ core-lib/src/main/kotlin/com/jacob/core_lib/word/Word.kt
@@ -1,5 +1,22 @@
 package com.jacob.core_lib.word
 
-open class Word(val value: Int) {
+open class Word(private val value: Int) {
+    override fun toString(): String {
+        return "Word(value=$value)"
+    }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Word
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
 }
