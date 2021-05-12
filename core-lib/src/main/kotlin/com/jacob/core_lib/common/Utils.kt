cb440f3e480bc557835448203f25294c6df11f0c
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-12 20:57:48 +0530

    Added extension functions to make it easier to convert strings to register address.

--- core-lib/src/main/kotlin/com/jacob/core_lib/common/Utils.kt
+++ core-lib/src/main/kotlin/com/jacob/core_lib/common/Utils.kt
@@ -1,6 +1,22 @@
 package com.jacob.core_lib.common
 
-fun Boolean.toInt() = if (this) 1 else 0
+import com.jacob.core_lib.common.addresses.RegisterAddress
+import kotlin.reflect.KFunction1
+
+private val pattern = Regex("R\\d+")
 
 fun <T> List<T?>.padListTill(newLength: Int): MutableList<T?> =
     List(maxOf(newLength, size)) { index -> getOrNull(index) }.toMutableList()
+
+fun <T> String.toRegisterAddress(convertToAddressOfType: KFunction1<RegisterAddress, T>): T {
+    require(pattern.matches(this))
+
+    return this.replace("R", "REGISTER_")
+        .let(RegisterAddress::valueOf)
+        .let(convertToAddressOfType)
+}
+
+fun List<String>.toRegisterAddresses(): List<RegisterAddress> {
+    return this.map { it.replace("R", "REGISTER_") }
+        .map(RegisterAddress::valueOf)
+}
