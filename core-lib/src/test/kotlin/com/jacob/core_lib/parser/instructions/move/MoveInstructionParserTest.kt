432665e4746d006083edd31581537daf63b8542b
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-12 21:32:19 +0530

    Added test for MoveInstructionParserTest.

---
+++ core-lib/src/test/kotlin/com/jacob/core_lib/parser/instructions/move/MoveInstructionParserTest.kt
@@ -0,0 +1,45 @@
+package com.jacob.core_lib.parser.instructions.move
+
+import com.jacob.core_lib.instructions.Instruction
+import com.jacob.core_lib.instructions.move.MoveImmediate
+import com.jacob.core_lib.instructions.move.MoveRegister
+import org.amshove.kluent.`should be instance of`
+import org.amshove.kluent.`should throw`
+import org.amshove.kluent.invoking
+import org.junit.jupiter.api.Test
+
+internal class MoveInstructionParserTest {
+    @Test
+    internal fun `creates correct instruction when given postive immediate value`() {
+        val instructionString = "MOV R1, #5"
+        val moveInstruction = MoveInstructionParser.from(instructionString)
+
+        moveInstruction `should be instance of` Instruction::class
+        moveInstruction `should be instance of` MoveImmediate::class
+    }
+
+    @Test
+    internal fun `creates correct instruction when given negative immediate value`() {
+        val instructionString = "MOV R1, #-5"
+        val moveInstruction = MoveInstructionParser.from(instructionString)
+
+        moveInstruction `should be instance of` Instruction::class
+        moveInstruction `should be instance of` MoveImmediate::class
+    }
+
+    @Test
+    internal fun `creates correct instruction when given register value`() {
+        val instructionString = "MOV R1, R5"
+        val moveInstruction = MoveInstructionParser.from(instructionString)
+
+        moveInstruction `should be instance of` Instruction::class
+        moveInstruction `should be instance of` MoveRegister::class
+    }
+
+    @Test
+    internal fun `throws exception if invalid register is given`() {
+        val instructionString = "MOV R1, R20"
+        invoking { MoveInstructionParser.from(instructionString) } `should throw` IllegalArgumentException::class
+
+    }
+}
