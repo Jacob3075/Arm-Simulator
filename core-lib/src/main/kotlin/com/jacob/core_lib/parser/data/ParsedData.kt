a0dee43009a649326ef4c3275e2c15f585c04f15
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-13 22:38:49 +0530

    Added helper function to convert ParsedData to Variables.

--- core-lib/src/main/kotlin/com/jacob/core_lib/parser/data/ParsedData.kt
+++ core-lib/src/main/kotlin/com/jacob/core_lib/parser/data/ParsedData.kt
@@ -1,3 +1,10 @@
 package com.jacob.core_lib.parser.data
 
+import com.jacob.core_lib.common.addresses.MemoryAddress
+import com.jacob.core_lib.core.Variable
+
 data class ParsedData(val variableName: String, val variableValue: Int)
+
+fun List<ParsedData>.toVariables() = this.mapIndexed { index, parsedData ->
+    Variable(parsedData.variableName, MemoryAddress(index))
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,18 @@
 import com.jacob.core_lib.core.Core
 import com.jacob.core_lib.core.Program
 import com.jacob.core_lib.parser.Parser
+import com.jacob.core_lib.parser.data.toVariables
 import java.io.File
 
 fun main() {
     val file = File("./InputFile.txt")
     val dataFromFile = Parser.parseDataFromFile(file)
+    val variables = dataFromFile.second
+        .toVariables()
 
-    val core = Core(program = Program(dataFromFile.first))
+    val core = Core(program = Program(dataFromFile.first, variables))
     core.runProgram()
+
     println(core.registerArray)
     println(core.memoryArray)
 }
