5eff00fbd57175dffd83e49d35b12b464eb01df2
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-13 13:09:34 +0530

    Added tests for StoreInstructionParser.

---
+++ core-lib/src/test/kotlin/com/jacob/core_lib/parser/instructions/store/StoreInstructionParserTest.kt
@@ -0,0 +1,44 @@
+package com.jacob.core_lib.parser.instructions.store
+
+import com.jacob.core_lib.instructions.store.StoreRegisterAddress
+import com.jacob.core_lib.instructions.store.StoreVariableAddress
+import org.amshove.kluent.`should be instance of`
+import org.amshove.kluent.`should throw`
+import org.amshove.kluent.invoking
+import org.junit.jupiter.api.Test
+
+internal class StoreInstructionParserTest {
+    @Test
+    internal fun `creates correct instruction when given single letter variable names`() {
+        val instructionString = "STR R1, =A"
+
+        val instruction = StoreInstructionParser.from(instructionString)
+
+        instruction `should be instance of` StoreVariableAddress::class
+    }
+
+    @Test
+    internal fun `creates correct instruction when given multiple letter variable names`() {
+        val instructionString = "STR R1, =ABC"
+
+        val instruction = StoreInstructionParser.from(instructionString)
+
+        instruction `should be instance of` StoreVariableAddress::class
+    }
+
+    @Test
+    internal fun `creates correct instruction when given valid register`() {
+        val instructionString = "STR R1, [R2]"
+
+        val instruction = StoreInstructionParser.from(instructionString)
+
+        instruction `should be instance of` StoreRegisterAddress::class
+    }
+
+    @Test
+    internal fun `throws exception of invalid register is given`() {
+        val instructionString = "STR R1, [R22]"
+
+        invoking { StoreInstructionParser.from(instructionString) } `should throw` IllegalArgumentException::class
+    }
+}
