121b5b5c67c9940cc12d6e7eb0bcd62c4b8ba04a
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-14 11:19:14 +0530

    Added tests to Core to read and execute the files.

--- core-lib/src/test/kotlin/com/jacob/core_lib/core/CoreTest.kt
+++ core-lib/src/test/kotlin/com/jacob/core_lib/core/CoreTest.kt
@@ -1,3 +1,55 @@
 package com.jacob.core_lib.core
 
-internal class CoreTest
+import com.jacob.core_lib.common.RA
+import com.jacob.core_lib.common.W
+import com.jacob.core_lib.getFile
+import com.jacob.core_lib.parser.Parser
+import com.jacob.core_lib.parser.data.toVariables
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+
+
+internal class CoreTest {
+    @Test
+    internal fun `test file 1`() {
+        val file = getFile("/TestInputFile1.txt")
+
+        val parseDataFromFile = Parser.parseDataFromFile(file)
+
+        val instructions = parseDataFromFile.first
+        val variables = parseDataFromFile.second.toVariables()
+
+        val program = Program(instructions, variables)
+        val core = Core(program = program)
+
+        core.runProgram()
+
+        core.apply {
+            registerArray.getRegisterAt(1.RA).getRegisterValue() `should be equal to` 5.W
+            registerArray.getRegisterAt(2.RA).getRegisterValue() `should be equal to` 10.W
+            registerArray.getRegisterAt(3.RA).getRegisterValue() `should be equal to` 15.W
+            registerArray.getRegisterAt(4.RA).getRegisterValue() `should be equal to` 10.W
+        }
+    }
+
+    @Test
+    internal fun `test file 2`() {
+        val file = getFile("/TestInputFile2.txt")
+
+        val parseDataFromFile = Parser.parseDataFromFile(file)
+
+        val instructions = parseDataFromFile.first
+        val variables = parseDataFromFile.second.toVariables()
+
+        val program = Program(instructions, variables)
+        val core = Core(program = program)
+
+        core.runProgram()
+
+        core.apply {
+            registerArray.getRegisterAt(1.RA).getRegisterValue() `should be equal to` 5.W
+            registerArray.getRegisterAt(3.RA).getRegisterValue() `should be equal to` 7.W
+            registerArray.getRegisterAt(4.RA).getRegisterValue() `should be equal to` (-2).W
+        }
+    }
+}
