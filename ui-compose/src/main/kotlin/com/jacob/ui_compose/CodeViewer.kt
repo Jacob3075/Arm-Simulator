3cc0b342954b5338543aad6bfd27e1802c6509c5
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-19 12:11:33 +0530

    Added ScrollBar to CodeViewer.

--- ui-compose/src/main/kotlin/com/jacob/ui_compose/CodeViewer.kt
+++ ui-compose/src/main/kotlin/com/jacob/ui_compose/CodeViewer.kt
@@ -1,15 +1,21 @@
 package com.jacob.ui_compose
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jacob.ui_compose.models.CodeViewerLine
@@ -18,13 +24,20 @@ import java.io.File
 @Composable
 fun CodeViewer(modifier: Modifier, file: File) {
     val codeLines = file.convertToCodeLines()
+    val state = rememberLazyListState()
 
-    Column(modifier = modifier.border(width = 1.dp, color = Color.DarkGray)) {
-        LazyColumn(modifier = Modifier.padding(8.dp).fillMaxSize()) {
+    Box(modifier = modifier.border(width = 1.dp, color = Color.DarkGray)) {
+        LazyColumn(modifier = Modifier.padding(8.dp).fillMaxSize(), state = state) {
             items(items = codeLines) {
                 Line(it)
             }
         }
+        Scrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            state = state,
+            itemCount = codeLines.size,
+            averageItemSize = 27.2.dp
+        )
     }
 }
 
@@ -67,5 +80,18 @@ private fun LineNumber(lineNumber: Int, modifier: Modifier) {
             fontSize = 12.sp
         ),
         modifier = modifier
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun Scrollbar(modifier: Modifier, state: LazyListState, itemCount: Int, averageItemSize: Dp) {
+    VerticalScrollbar(
+        modifier = modifier,
+        adapter = rememberScrollbarAdapter(
+            scrollState = state,
+            itemCount = itemCount,
+            averageItemSize = averageItemSize
+        )
     )
 }
