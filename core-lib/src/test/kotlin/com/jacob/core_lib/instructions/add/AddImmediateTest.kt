b85e3ff9b6f0a39499c64ea202e71eb52cb04b9e
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-24 21:59:36 +0530

    Updated tests for AddImmediate.

--- core-lib/src/test/kotlin/com/jacob/core_lib/instructions/add/AddImmediateTest.kt
+++ core-lib/src/test/kotlin/com/jacob/core_lib/instructions/add/AddImmediateTest.kt
@@ -1,33 +1,19 @@
 package com.jacob.core_lib.instructions.add
 
-import com.jacob.core_lib.common.addresses.RegisterAddress
+import com.jacob.core_lib.common.I
+import com.jacob.core_lib.common.RA
+import com.jacob.core_lib.common.W
+import com.jacob.core_lib.common.addresses.DestinationRegister
+import com.jacob.core_lib.common.addresses.SourceRegister
 import com.jacob.core_lib.core.*
 import com.jacob.core_lib.createAddInstruction
 import com.jacob.core_lib.instructions.Instruction
+import com.jacob.core_lib.instructions.shift.LeftShift
 import com.jacob.core_lib.parser.data.ParsedData
-import com.jacob.core_lib.word.Word
 import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.`should be instance of`
-import org.amshove.kluent.shouldNotBeNull
 import org.junit.jupiter.api.Test
 
-// TODO: ADD TESTS WITH SHIFT OPERATIONS
 internal class AddImmediateTest {
-
-    @Test
-    internal fun `can create new add instruction with source register and immediate value`() {
-        val sourceRegister1 = RegisterAddress.REGISTER_0
-        val destinationRegister = RegisterAddress.REGISTER_2
-        val immediateValue = 10
-
-        val addInstruction = createAddInstruction(destinationRegister, sourceRegister1, immediateValue)
-
-        addInstruction.shouldNotBeNull()
-        addInstruction `should be instance of` AddImmediate::class
-        addInstruction `should be instance of` Add::class
-        addInstruction `should be instance of` Instruction::class
-    }
-
     @Test
     internal fun `executing add instruction with source register and immediate value reads and updates the correct registers`() {
         val memoryArray = MemoryArray()
@@ -42,18 +28,17 @@ internal class AddImmediateTest {
             variables = variables
         )
 
-        registerArray.setValueAtRegister(RegisterAddress.REGISTER_0, Word(10))
+        registerArray.setValueAtRegister(0.RA, 10.W)
 
-        val sourceRegister1 = RegisterAddress.REGISTER_0
-        val destinationRegister = RegisterAddress.REGISTER_2
+        val sourceRegister1 = 0.RA
+        val destinationRegister = 2.RA
         val immediateValue = 20
 
         val addInstruction = createAddInstruction(destinationRegister, sourceRegister1, immediateValue)
 
         addInstruction.execute(executionEnvironment)
 
-        registerArray.getRegisterAt(destinationRegister)
-            .getRegisterValue() `should be equal to` Word(30)
+        registerArray.getRegisterAt(destinationRegister).getRegisterValue() `should be equal to` 30.W
     }
 
     @Test
@@ -62,13 +47,13 @@ internal class AddImmediateTest {
         val registerArray = RegisterArray()
         val variables = listOf<ParsedData>()
 
-        val registerAddress1 = RegisterAddress.REGISTER_1
-        val registerAddress2 = RegisterAddress.REGISTER_2
-        val registerAddress3 = RegisterAddress.REGISTER_3
-        val registerAddress4 = RegisterAddress.REGISTER_4
+        val registerAddress1 = 1.RA
+        val registerAddress2 = 2.RA
+        val registerAddress3 = 3.RA
+        val registerAddress4 = 4.RA
 
-        registerArray.setValueAtRegister(registerAddress1, Word(10))
-        registerArray.setValueAtRegister(registerAddress2, Word(20))
+        registerArray.setValueAtRegister(registerAddress1, 10.W)
+        registerArray.setValueAtRegister(registerAddress2, 20.W)
 
         val add1 = createAddInstruction(registerAddress3, registerAddress1, 20)
         val add2 = createAddInstruction(registerAddress4, registerAddress2, 30)
@@ -83,25 +68,48 @@ internal class AddImmediateTest {
 
         core.runProgram()
 
-        registerArray.getRegisterAt(registerAddress1)
-            .getRegisterValue() `should be equal to` Word(10)
+        registerArray.getRegisterAt(registerAddress3).getRegisterValue() `should be equal to` 30.W
+        registerArray.getRegisterAt(registerAddress4).getRegisterValue() `should be equal to` 50.W
+    }
 
-        registerArray.getRegisterAt(registerAddress2)
-            .getRegisterValue() `should be equal to` Word(20)
+    @Test
+    internal fun `running add instructions using immediate value with left shift reads and updates the correct registers`() {
+        val memoryArray = MemoryArray()
+        val registerArray = RegisterArray()
+        val variables = listOf<ParsedData>()
 
-        registerArray.getRegisterAt(registerAddress3)
-            .getRegisterValue() `should be equal to` Word(30)
+        val registerAddress1 = 1.RA
+        val registerAddress2 = 2.RA
+        val registerAddress3 = 3.RA
+        val registerAddress4 = 4.RA
 
-        registerArray.getRegisterAt(registerAddress4)
-            .getRegisterValue() `should be equal to` Word(50)
+        registerArray.setValueAtRegister(registerAddress1, 10.W)
+        registerArray.setValueAtRegister(registerAddress2, 20.W)
 
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_5).getRegisterValue() `should be equal to` Word(0)
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_6).getRegisterValue() `should be equal to` Word(0)
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_7).getRegisterValue() `should be equal to` Word(0)
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_8).getRegisterValue() `should be equal to` Word(0)
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_9).getRegisterValue() `should be equal to` Word(0)
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_10).getRegisterValue() `should be equal to` Word(0)
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_11).getRegisterValue() `should be equal to` Word(0)
-        registerArray.getRegisterAt(RegisterAddress.REGISTER_12).getRegisterValue() `should be equal to` Word(0)
+        val add1 = Add.of(
+            destinationRegister = DestinationRegister(registerAddress3),
+            sourceRegister1 = SourceRegister(registerAddress1),
+            immediateValue = 10.I,
+            shiftOperation = LeftShift(1)
+        )
+        val add2 = Add.of(
+            destinationRegister = DestinationRegister(registerAddress4),
+            sourceRegister1 = SourceRegister(registerAddress2),
+            immediateValue = 10.I,
+            shiftOperation = LeftShift(3)
+        )
+
+        val instructions: List<Instruction> = listOf(
+            add1,
+            add2,
+        )
+        val program = Program(instructions, variables)
+
+        val core = Core(memoryArray, registerArray, program)
+
+        core.runProgram()
+
+        registerArray.getRegisterAt(registerAddress3).getRegisterValue() `should be equal to` 30.W
+        registerArray.getRegisterAt(registerAddress4).getRegisterValue() `should be equal to` 100.W
     }
 }
