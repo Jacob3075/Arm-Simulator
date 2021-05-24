3e5a495efdf5bd2738a33f05b9dff807b91cbc78
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-24 22:57:44 +0530

    Added tests for RightShiftOperationParser.

---
+++ core-lib/src/test/kotlin/com/jacob/core_lib/parser/instructions/shift/operation/RightShiftOperationParserTest.kt
@@ -0,0 +1,22 @@
+package com.jacob.core_lib.parser.instructions.shift.operation
+
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+
+internal class RightShiftOperationParserTest {
+    @Test
+    internal fun `creates correct shift operation 1`() {
+        val operationString = "LSR #4"
+        val rightShift = RightShiftOperationParser(operationString).parse()
+
+        rightShift.shiftAmount `should be equal to` 4
+    }
+
+    @Test
+    internal fun `creates correct shift operation 2`() {
+        val operationString = "LSR #14"
+        val rightShift = RightShiftOperationParser(operationString).parse()
+
+        rightShift.shiftAmount `should be equal to` 14
+    }
+}
