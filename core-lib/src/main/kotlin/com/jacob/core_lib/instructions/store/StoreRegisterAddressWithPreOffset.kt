0a88d968e3ae03b4038b5dca9818d26f8fbce95c
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-28 11:03:48 +0530

    Completed StoreRegisterAddressWithPreOffset and its tests.

--- core-lib/src/main/kotlin/com/jacob/core_lib/instructions/store/StoreRegisterAddressWithPreOffset.kt
+++ core-lib/src/main/kotlin/com/jacob/core_lib/instructions/store/StoreRegisterAddressWithPreOffset.kt
@@ -1,16 +1,25 @@
 package com.jacob.core_lib.instructions.store
 
+import com.jacob.core_lib.common.MA
 import com.jacob.core_lib.common.addresses.DestinationRegister
 import com.jacob.core_lib.common.addresses.SourceRegister
 import com.jacob.core_lib.core.ExecutionEnvironment
 import com.jacob.core_lib.word.ImmediateValue
 
-class StoreRegisterAddressWithPreOffset(
+data class StoreRegisterAddressWithPreOffset(
     internal val sourceRegister: SourceRegister,
     internal val destinationRegister: DestinationRegister,
     internal val offset: ImmediateValue
 ) : Store {
     override fun execute(executionEnvironment: ExecutionEnvironment) {
-        TODO("Not yet implemented")
+        val (registerArray, memoryArray) = executionEnvironment
+
+        val destinationAddress = registerArray.getRegisterAt(destinationRegister).getRegisterValue()
+        val wordToStore = registerArray.getRegisterAt(sourceRegister).getRegisterValue()
+
+        val resultAddress = destinationAddress + offset
+
+        memoryArray.setWordAt(resultAddress.value.MA, wordToStore)
+        registerArray.setValueAtRegister(destinationRegister, resultAddress)
     }
 }
---
+++ core-lib/src/test/kotlin/com/jacob/core_lib/instructions/store/StoreRegisterAddressWithPreOffsetTest.kt
@@ -0,0 +1,46 @@
+package com.jacob.core_lib.instructions.store
+
+import com.jacob.core_lib.common.*
+import com.jacob.core_lib.getExecutionEnvironment
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+
+internal class StoreRegisterAddressWithPreOffsetTest {
+    @Test
+    internal fun `reads and updates correct memory address when given positive offset value`() {
+        val sourceRegister = 1.SR
+        val destinationRegister = 0.DR
+        val offset = 3.I
+
+        val executionEnvironment = getExecutionEnvironment()
+        val (registerArray, memoryArray) = executionEnvironment
+
+        registerArray.setValueAtRegister(1.SR, 10.W)
+        registerArray.setValueAtRegister(0.DR, 6.W)
+
+        val loadInstruction = StoreRegisterAddressWithPreOffset(sourceRegister, destinationRegister, offset)
+        loadInstruction.execute(executionEnvironment)
+
+        memoryArray.getWordAt(9.MA) `should be equal to` 10.W
+        registerArray.getRegisterAt(0.DR).getRegisterValue() `should be equal to` 9.W
+    }
+
+    @Test
+    internal fun `reads and updates correct memory address when given negative offset value`() {
+        val sourceRegister = 1.SR
+        val destinationRegister = 0.DR
+        val offset = (-3).I
+
+        val executionEnvironment = getExecutionEnvironment()
+        val (registerArray, memoryArray) = executionEnvironment
+
+        registerArray.setValueAtRegister(1.SR, 10.W)
+        registerArray.setValueAtRegister(0.DR, 6.W)
+
+        val loadInstruction = StoreRegisterAddressWithPreOffset(sourceRegister, destinationRegister, offset)
+        loadInstruction.execute(executionEnvironment)
+
+        memoryArray.getWordAt(3.MA) `should be equal to` 10.W
+        registerArray.getRegisterAt(0.DR).getRegisterValue() `should be equal to` 3.W
+    }
+}
