0a57e8d56512af26560f8f06b117704864817fd8
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-05-30 11:39:33 +0530

    Added default method in Instruction to add conditional execution to instruction.

--- core-lib/src/main/kotlin/com/jacob/core_lib/instructions/Instruction.kt
+++ core-lib/src/main/kotlin/com/jacob/core_lib/instructions/Instruction.kt
@@ -1,7 +1,18 @@
 package com.jacob.core_lib.instructions
 
 import com.jacob.core_lib.core.ExecutionEnvironment
+import com.jacob.core_lib.instructions.conditionals.Always
+import com.jacob.core_lib.instructions.conditionals.Conditionals
+import com.jacob.core_lib.instructions.conditionals.Conditionals.*
+import com.jacob.core_lib.instructions.conditionals.Equal
 
 interface Instruction {
     fun execute(executionEnvironment: ExecutionEnvironment)
+    fun withConditional(conditional: Conditionals): Instruction = when (conditional) {
+        AL -> Always(this)
+        EQ -> Equal(this)
+        NE -> TODO()
+        CS -> TODO()
+        CC -> TODO()
+    }
 }
