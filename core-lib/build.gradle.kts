8a94bc4d6f0199891391d9b3bff313965251c9b0
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-06-02 11:53:50 +0530

    Converted ParserTest to parameterized test and added dependency.

--- core-lib/build.gradle.kts
+++ core-lib/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
     testImplementation("org.amshove.kluent:kluent:1.65")
     testImplementation("io.mockk:mockk:1.10.6")
 }
--- core-lib/src/test/kotlin/com/jacob/core_lib/parser/ParserTest.kt
+++ core-lib/src/test/kotlin/com/jacob/core_lib/parser/ParserTest.kt
@@ -2,142 +2,46 @@ package com.jacob.core_lib.parser
 
 import com.jacob.core_lib.core.*
 import com.jacob.core_lib.getFile
+import com.jacob.core_lib.instructions.Instruction
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should be in`
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class ParserTest {
-    @Test
-    internal fun `test file 1`() {
-        val file = getFile("/TestInputFile1.s")
-
-        val parseDataFromFile = Parser.parseDataFromFile(file)
-
-        val instructions = parseDataFromFile.first
-        val variables = parseDataFromFile.second.toVariables()
-
-        instructions.size `should be equal to` test1Data.first.size
-        variables.size `should be equal to` test1Data.second.size
-
-        test1Data.first.forEach { it `should be in` instructions }
-        test1Data.second.toVariables().forEach { it `should be in` variables }
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        fun testInput() = listOf(
+            Arguments.of("/TestInputFile1.s", test1Data),
+            Arguments.of("/TestInputFile2.s", test2Data),
+            Arguments.of("/TestInputFile3.s", test3Data),
+            Arguments.of("/TestInputFile4.s", test4Data),
+            Arguments.of("/TestInputFile5.s", test5Data),
+            Arguments.of("/TestInputFile6.s", test6Data),
+            Arguments.of("/TestInputFile7.s", test7Data),
+            Arguments.of("/TestInputFile8.s", test8Data),
+        )
     }
 
-    @Test
-    internal fun `test file 2`() {
-        val file = getFile("/TestInputFile2.s")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("testInput")
+    internal fun `test parser for input files`(fileName: String, testData: Pair<List<Instruction>, List<ParsedData>>) {
+        val file = getFile(fileName)
 
         val parseDataFromFile = Parser.parseDataFromFile(file)
 
         val instructions = parseDataFromFile.first
         val variables = parseDataFromFile.second.toVariables()
 
-        instructions.size `should be equal to` test2Data.first.size
-        variables.size `should be equal to` test2Data.second.size
+        instructions.size `should be equal to` testData.first.size
+        variables.size `should be equal to` testData.second.size
 
-        test2Data.first.forEach { it `should be in` instructions }
-        test2Data.second.toVariables().forEach { it `should be in` variables }
-    }
+        testData.first.forEach { it `should be in` instructions }
+        testData.second.toVariables().forEach { it `should be in` variables }
 
-    @Test
-    internal fun `test file 3`() {
-        val file = getFile("/TestInputFile3.s")
-
-        val parseDataFromFile = Parser.parseDataFromFile(file)
-
-        val instructions = parseDataFromFile.first
-        val variables = parseDataFromFile.second.toVariables()
-
-        instructions.size `should be equal to` test3Data.first.size
-        variables.size `should be equal to` test3Data.second.size
-
-        test3Data.first.forEach { it `should be in` instructions }
-        test3Data.second.toVariables().forEach { it `should be in` variables }
-    }
-
-    @Test
-    internal fun `test file 4`() {
-        val file = getFile("/TestInputFile4.s")
-
-        val parseDataFromFile = Parser.parseDataFromFile(file)
-
-        val instructions = parseDataFromFile.first
-        val variables = parseDataFromFile.second.toVariables()
-
-        instructions.size `should be equal to` test4Data.first.size
-        variables.size `should be equal to` test4Data.second.size
-
-        test4Data.first.forEach { it `should be in` instructions }
-        test4Data.second.toVariables().forEach { it `should be in` variables }
-    }
-
-    @Test
-    internal fun `test file 5`() {
-        val file = getFile("/TestInputFile5.s")
-
-        val parseDataFromFile = Parser.parseDataFromFile(file)
-
-        val instructions = parseDataFromFile.first
-        val variables = parseDataFromFile.second.toVariables()
-
-        instructions.size `should be equal to` test5Data.first.size
-        variables.size `should be equal to` test5Data.second.size
-
-        test5Data.first.forEach { it `should be in` instructions }
-        test5Data.second.toVariables().forEach { it `should be in` variables }
-    }
-
-    @Test
-    internal fun `test file 6`() {
-        val file = getFile("/TestInputFile6.s")
-
-        val parseDataFromFile = Parser.parseDataFromFile(file)
-
-        val instructions = parseDataFromFile.first
-        val variables = parseDataFromFile.second.toVariables()
-
-        instructions.size `should be equal to` test6Data.first.size
-        variables.size `should be equal to` test6Data.second.size
-
-        test6Data.first.forEach { it `should be in` instructions }
-        test6Data.second.toVariables().forEach { it `should be in` variables }
-    }
-
-    @Test
-    internal fun `test file 7`() {
-        val file = getFile("/TestInputFile7.s")
-
-        val parseDataFromFile = Parser.parseDataFromFile(file)
-
-        val instructions = parseDataFromFile.first
-        val variables = parseDataFromFile.second.toVariables()
-
-        instructions.size `should be equal to` test7Data.first.size
-        variables.size `should be equal to` test7Data.second.size
-
-        test7Data.first.forEach { it `should be in` instructions }
-        test7Data.second.toVariables().forEach { it `should be in` variables }
-
-        instructions `should be equal to` test7Data.first
-        variables `should be equal to` test7Data.second.toVariables()
-    }
-
-    @Test
-    internal fun `test file 8`() {
-        val file = getFile("/TestInputFile8.s")
-
-        val parseDataFromFile = Parser.parseDataFromFile(file)
-
-        val instructions = parseDataFromFile.first
-        val variables = parseDataFromFile.second.toVariables()
-
-        instructions.size `should be equal to` test8Data.first.size
-        variables.size `should be equal to` test8Data.second.size
-
-        test8Data.first.forEach { it `should be in` instructions }
-        test8Data.second.toVariables().forEach { it `should be in` variables }
-
-        instructions `should be equal to` test8Data.first
-        variables `should be equal to` test8Data.second.toVariables()
+        instructions `should be equal to` testData.first
+        variables `should be equal to` testData.second.toVariables()
     }
 }
