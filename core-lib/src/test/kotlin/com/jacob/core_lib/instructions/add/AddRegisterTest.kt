6d78cc91c2d635fe882241b4fb44e0c6c8454c37
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-06-02 15:32:00 +0530

    Removed redundant tests.

--- core-lib/src/test/kotlin/com/jacob/core_lib/instructions/add/AddRegisterTest.kt
+++ core-lib/src/test/kotlin/com/jacob/core_lib/instructions/add/AddRegisterTest.kt
@@ -1,12 +1,10 @@
 package com.jacob.core_lib.instructions.add
 
-import com.jacob.core_lib.common.*
-import com.jacob.core_lib.core.RegisterArray
+import com.jacob.core_lib.common.DR
+import com.jacob.core_lib.common.SR
+import com.jacob.core_lib.common.W
 import com.jacob.core_lib.getExecutionEnvironment
-import com.jacob.core_lib.instructions.conditionals.Conditionals
-import com.jacob.core_lib.registers.StatusRegister
 import org.amshove.kluent.`should be equal to`
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class AddRegisterTest {
@@ -60,64 +58,5 @@ internal class AddRegisterTest {
         addInstruction.execute(executionEnvironment)
 
         executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` (-25).W
-    }
-
-    @Nested
-    inner class WithShiftOperations {
-        @Test
-        internal fun `applying left shift to register with positive value`() {
-            val executionEnvironment = getExecutionEnvironment().apply {
-                registerArray.setValueAtRegister(1.DR, 15.W)
-                registerArray.setValueAtRegister(2.DR, 10.W)
-            }
-
-            val addInstruction = Add.of(3.DR, 1.SR, 2.SR, 2.LS)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` 55.W
-        }
-
-        @Test
-        internal fun `applying right shift to register with negative value`() {
-            val executionEnvironment = getExecutionEnvironment().apply {
-                registerArray.setValueAtRegister(1.DR, 10.W)
-                registerArray.setValueAtRegister(2.DR, (-12).W)
-            }
-
-            val addInstruction = Add.of(3.DR, 1.SR, 2.SR, 2.RS)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` 1073741831.W
-        }
-    }
-
-    @Nested
-    inner class WithConditionals {
-        @Test
-        internal fun `instruction with equals conditional and its satisfied`() {
-            val registerArray = RegisterArray(statusRegister = StatusRegister(zero = true))
-            val executionEnvironment = getExecutionEnvironment(registerArray = registerArray).apply {
-                registerArray.setValueAtRegister(1.DR, 10.W)
-                registerArray.setValueAtRegister(2.DR, 20.W)
-            }
-
-            val addInstruction = Add.of(3.DR, 1.SR, 2.SR, conditional = Conditionals.EQ)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` 30.W
-        }
-
-        @Test
-        internal fun `instruction with equals conditional and its not satisfied`() {
-            val executionEnvironment = getExecutionEnvironment().apply {
-                registerArray.setValueAtRegister(1.DR, 10.W)
-                registerArray.setValueAtRegister(2.DR, 20.W)
-            }
-
-            val addInstruction = Add.of(3.DR, 1.SR, 2.SR, conditional = Conditionals.EQ)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` 0.W
-        }
     }
 }
--- core-lib/src/test/kotlin/com/jacob/core_lib/instructions/sub/SubRegisterTest.kt
+++ core-lib/src/test/kotlin/com/jacob/core_lib/instructions/sub/SubRegisterTest.kt
@@ -1,12 +1,10 @@
 package com.jacob.core_lib.instructions.sub
 
-import com.jacob.core_lib.common.*
-import com.jacob.core_lib.core.RegisterArray
+import com.jacob.core_lib.common.DR
+import com.jacob.core_lib.common.SR
+import com.jacob.core_lib.common.W
 import com.jacob.core_lib.getExecutionEnvironment
-import com.jacob.core_lib.instructions.conditionals.Conditionals
-import com.jacob.core_lib.registers.StatusRegister
 import org.amshove.kluent.`should be equal to`
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class SubRegisterTest {
@@ -60,65 +58,5 @@ internal class SubRegisterTest {
         addInstruction.execute(executionEnvironment)
 
         executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` (-5).W
-    }
-
-    @Nested
-    inner class WithShiftOperations {
-        @Test
-        internal fun `applying left shift to register with positive value`() {
-            val executionEnvironment = getExecutionEnvironment().apply {
-                registerArray.setValueAtRegister(1.DR, 15.W)
-                registerArray.setValueAtRegister(2.DR, 10.W)
-            }
-
-            val addInstruction = Sub.of(3.DR, 1.SR, 2.SR, 2.LS)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` (-25).W
-        }
-
-        @Test
-        internal fun `applying right shift to register with negative value`() {
-            val executionEnvironment = getExecutionEnvironment().apply {
-                registerArray.setValueAtRegister(1.DR, 10.W)
-                registerArray.setValueAtRegister(2.DR, (-12).W)
-            }
-
-            val addInstruction = Sub.of(3.DR, 1.SR, 2.SR, 2.RS)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR)
-                .getRegisterValue() `should be equal to` (-1073741811).W
-        }
-    }
-
-    @Nested
-    inner class WithConditionals {
-        @Test
-        internal fun `instruction with equals conditional and its satisfied`() {
-            val registerArray = RegisterArray(statusRegister = StatusRegister(zero = true))
-            val executionEnvironment = getExecutionEnvironment(registerArray = registerArray).apply {
-                registerArray.setValueAtRegister(1.DR, 20.W)
-                registerArray.setValueAtRegister(2.DR, 10.W)
-            }
-
-            val addInstruction = Sub.of(3.DR, 1.SR, 2.SR, conditional = Conditionals.EQ)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` 10.W
-        }
-
-        @Test
-        internal fun `instruction with equals conditional and its not satisfied`() {
-            val executionEnvironment = getExecutionEnvironment().apply {
-                registerArray.setValueAtRegister(1.DR, 10.W)
-                registerArray.setValueAtRegister(2.DR, 20.W)
-            }
-
-            val addInstruction = Sub.of(3.DR, 1.SR, 2.SR, conditional = Conditionals.EQ)
-            addInstruction.execute(executionEnvironment)
-
-            executionEnvironment.registerArray.getRegisterAt(3.DR).getRegisterValue() `should be equal to` 0.W
-        }
     }
 }
